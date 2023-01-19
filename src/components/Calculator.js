dd66bd009a7d3a4daeb27a7d5b14573e17e0a1ac
M.Nisar Ali <101400298+MNisarAli@users.noreply.github.com>
2023-01-19 20:38:53 +0500

    Refactor component class to function with hooks.

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,47 +1,42 @@
-import React, { PureComponent } from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 
-export default class Calculator extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: '0',
-      operation: null,
-      next: null,
-    };
-  }
+export default function Calculator() {
+  const [state, setState] = useState({
+    total: '0',
+    operation: null,
+    next: null,
+  });
 
-  newOperation = (event) => {
+  const newOperation = (event) => {
     const targetButton = event.target.innerText;
-    const getResult = calculate(this.state, targetButton);
-    this.setState(getResult);
+    const getResult = calculate(state, targetButton);
+    setState(getResult);
   };
 
-  render() {
-    const { total, operation, next } = this.state;
-    return (
-      <section className="calc-app">
-        <input className="calc-result" type="text" value={`${total || ''} ${operation || ''} ${next || ''}`} />
-        <button type="button" onClick={this.newOperation}>AC</button>
-        <button type="button" onClick={this.newOperation}>+/-</button>
-        <button type="button" onClick={this.newOperation}>%</button>
-        <button type="button" className="operator" onClick={this.newOperation}>÷</button>
-        <button type="button" onClick={this.newOperation}>7</button>
-        <button type="button" onClick={this.newOperation}>8</button>
-        <button type="button" onClick={this.newOperation}>9</button>
-        <button type="button" className="operator" onClick={this.newOperation}>x</button>
-        <button type="button" onClick={this.newOperation}>4</button>
-        <button type="button" onClick={this.newOperation}>5</button>
-        <button type="button" onClick={this.newOperation}>6</button>
-        <button type="button" className="operator" onClick={this.newOperation}>-</button>
-        <button type="button" onClick={this.newOperation}>1</button>
-        <button type="button" onClick={this.newOperation}>2</button>
-        <button type="button" onClick={this.newOperation}>3</button>
-        <button type="button" className="operator" onClick={this.newOperation}>+</button>
-        <button type="button" className="zero" onClick={this.newOperation}>0</button>
-        <button type="button" onClick={this.newOperation}>.</button>
-        <button type="button" className="operator" onClick={this.newOperation}>=</button>
-      </section>
-    );
-  }
+  const { total, operation, next } = state;
+  return (
+    <section className="calc-app">
+      <input className="calc-result" type="text" value={`${total || ''} ${operation || ''} ${next || ''}`} />
+      <button type="button" onClick={newOperation}>AC</button>
+      <button type="button" onClick={newOperation}>+/-</button>
+      <button type="button" onClick={newOperation}>%</button>
+      <button type="button" className="operator" onClick={newOperation}>÷</button>
+      <button type="button" onClick={newOperation}>7</button>
+      <button type="button" onClick={newOperation}>8</button>
+      <button type="button" onClick={newOperation}>9</button>
+      <button type="button" className="operator" onClick={newOperation}>x</button>
+      <button type="button" onClick={newOperation}>4</button>
+      <button type="button" onClick={newOperation}>5</button>
+      <button type="button" onClick={newOperation}>6</button>
+      <button type="button" className="operator" onClick={newOperation}>-</button>
+      <button type="button" onClick={newOperation}>1</button>
+      <button type="button" onClick={newOperation}>2</button>
+      <button type="button" onClick={newOperation}>3</button>
+      <button type="button" className="operator" onClick={newOperation}>+</button>
+      <button type="button" className="zero" onClick={newOperation}>0</button>
+      <button type="button" onClick={newOperation}>.</button>
+      <button type="button" className="operator" onClick={newOperation}>=</button>
+    </section>
+  );
 }
