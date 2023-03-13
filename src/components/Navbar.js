a4eb19788fcba19225608ba18fdf7bccbed8dc75
M.Nisar Ali <101400298+MNisarAli@users.noreply.github.com>
2023-03-13 10:00:49 +0500

    Add menu toggle functionality for samller screens

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
-const Navbar = () => (
-  <nav className="navbar">
-    <h2>Math Magicians</h2>
-    <ul className="nav-list">
-      <li>
-        <NavLink exact="true" to="/" activeClassName="active">Home</NavLink>
-      </li>
-      <li>
-        <NavLink to="/calculator" activeClassName="active">Calculator</NavLink>
-      </li>
-      <li>
-        <NavLink to="/quotes" activeClassName="active">Quotes</NavLink>
-      </li>
-    </ul>
-  </nav>
-);
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <header className="navbar">
+      <h2>Math Magicians</h2>
+      <nav>
+        <button className={`menu_btn ${isOpen ? 'menu_btn_active' : ''}`} type="button" onClick={toggleMenu}>
+          <span />
+        </button>
+
+        <ul className={`menu_box ${isOpen ? 'menu_box_open' : ''}`}>
+          <li>
+            <NavLink exact="true" to="/" activeclassname="active" className="menu_item" onClick={toggleMenu}>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/calculator" activeclassname="active" className="menu_item" onClick={toggleMenu}>
+              Calculator
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/quotes" activeclassname="active" className="menu_item" onClick={toggleMenu}>
+              Quotes
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Navbar;
