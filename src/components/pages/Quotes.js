24ebc5a798d0b33bb7a1c86c2d0b02ee3f3c756c
M.Nisar Ali <101400298+MNisarAli@users.noreply.github.com>
2023-03-13 10:53:53 +0500

    Refactor Quotes component to display multiple quotes

--- src/components/pages/Quotes.js
+++ src/components/pages/Quotes.js
@@ -1,12 +1,41 @@
 import React from 'react';
 
-const Quotes = () => {
-  const quote = 'Mathematics is not about numbers, equations, computations, or algorithms: it is about understanding. -William Paul Thurston';
-  return (
-    <section className="quotes">
-      <p className="quote">{quote}</p>
-    </section>
-  );
-};
+const quotes = [
+  {
+    quote: 'Mathematics is not about numbers, equations, computations, or algorithms: it is about understanding.',
+    author: 'William Paul Thurston',
+  },
+  {
+    quote: 'Pure mathematics is, in its way, the poetry of logical ideas.',
+    author: 'Albert Einstein',
+  },
+  {
+    quote: 'The only way to learn mathematics is to do mathematics.',
+    author: 'Paul Halmos',
+  },
+  {
+    quote: 'Mathematics is the music of reason.',
+    author: 'James Joseph Sylvester',
+  },
+  {
+    quote: 'Without mathematics, there’s nothing you can do. Everything around you is mathematics. Everything around you is numbers.',
+    author: 'Shakuntala Devi',
+  },
+];
+
+const Quotes = () => (
+  <section className="quotes">
+    {quotes.map((quoteObj) => (
+      <p key={quoteObj.quote} className="quote">
+        {quoteObj.quote}
+        <br />
+        <span className="author">
+          _
+          {quoteObj.author}
+        </span>
+      </p>
+    ))}
+  </section>
+);
 
 export default Quotes;
